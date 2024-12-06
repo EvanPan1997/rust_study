ae011127d4d532d855c8c2bcba529c8f183ae9e0
yifei.pan <yifei.pan@geely.com>
2024-12-06 15:55:51 +0800

    initialize

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod math;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", math::add(1, 2));
+    math::next::test();
+}
---
+++ src/math/mod.rs
@@ -0,0 +1,5 @@
+pub mod next;
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
